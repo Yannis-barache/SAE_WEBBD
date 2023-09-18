761c2c16cd5ddee3e3f2d1f8330dce9a5cb9eb7b
Yannis-barache <ybarache@icloud.com>
2023-09-18 17:22:57 +0200

    Base de l'inscription et de la connexion

--- src/Site/views.py
+++ src/Site/views.py
@@ -7,23 +7,23 @@ from flask import render_template
 def home():
     return render_template(
         "home.html",
-        title="Hello World !!!!!",
+        title="Bienvenue sur le site du Festiut'O",
         names=["Pierre", "Paul", "Corinne"])
 
-@app.route("/page2")
+@app.route("/connexion")
 
 def page2():
     return render_template(
-        "page2.html",
-        title="Page 2",
-        message="Hello World !!!!!  Khalil et Yassine ont fait cette page de golmon",
+        "connexion.html",
+        title="Page de connexion",
+        message="Veuillez rentrer vos identifiants",
         )
 
 
-@app.route("/books")
-def books():
+@app.route("/inscription")
+def inscription():
     return render_template(
-        "livre.html",
-        title="Books",
-        books="livre de khabox",
+        "inscription.html",
+        title="Page d'inscription",
+        message= "Veuillez vous inscrire",
     )
